cbbe052cc67f0f355675668b064c228ec330f3d3
smalaca <ab13.krakow@gmail.com>
2023-09-23 11:27:05 +0200

    GREEN - add product to assortment

--- src/main/java/com/smalaca/productmanagement/application/assortment/AssortmentApplicationService.java
+++ src/main/java/com/smalaca/productmanagement/application/assortment/AssortmentApplicationService.java
@@ -1,5 +1,6 @@
 package com.smalaca.productmanagement.application.assortment;
 
+import com.smalaca.productmanagement.domain.assortment.Assortment;
 import com.smalaca.productmanagement.domain.assortment.AssortmentRepository;
 
 class AssortmentApplicationService {
@@ -13,9 +14,11 @@ class AssortmentApplicationService {
         // tłumaczenie na język biznesowy -> 0...*
         // 1. zamiana typów prostych na Value Object
         // 2. zamiana identyfikatorów na Aggregate
+        Assortment assortment = assortmentRepository.find(command.sellerId());
 
         // wywołanie metody na domenie - 1
 
         // zapis zmian -> 1 ... *
+        assortmentRepository.save(assortment);
     }
 }
